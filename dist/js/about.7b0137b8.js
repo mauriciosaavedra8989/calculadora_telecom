38d217aa2d5cf231c524b064a81b6c4c1ee61dec
lennydeveloper <iamlenny.dev@gmail.com>
2022-09-08 23:37:49 -0500

    Initial commit

---
+++ dist/js/about.7b0137b8.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkcalc_tel"]=self["webpackChunkcalc_tel"]||[]).push([[443],{5399:(t,l,s)=>{s.r(l),s.d(l,{default:()=>r});var a=function(){var t=this;t._self._c;return t._m(0)},e=[function(){var t=this,l=t._self._c;return l("div",{staticClass:"about"},[l("h1",[t._v("This is an about page")])])}],u=s(1001),c={},n=(0,u.Z)(c,a,e,!1,null,null,null);const r=n.exports}}]);
+//# sourceMappingURL=about.7b0137b8.js.map
